5c2faecbd8d094f615d5756a022508ca25fda35c
Luther <luther1096@gmail.com>
2025-02-24 12:52:03 +0530

    created a custom context provider and hook

--- src/App-v1.js
+++ src/App-v1.js
@@ -1,10 +1,40 @@
-import { useEffect, useState } from "react";
-import { createRandomPost, usePosts, PostContextProvider } from "./PostContext";
+import { createContext, useContext, useEffect, useState } from "react";
+import { faker } from "@faker-js/faker";
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
+
+// 1) Create a context
+const PostContext = createContext();
 
 function App() {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
   const [isFakeDark, setIsFakeDark] = useState(false);
 
   // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
 
   // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
   useEffect(
@@ -16,7 +46,15 @@ function App() {
 
   return (
     // 2) Provide the context value
-    <PostContextProvider>
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        onClearPosts: handleClearPosts,
+        onAddPost: handleAddPost,
+        searchQuery,
+        setSearchQuery,
+      }}
+    >
       <section>
         <button
           onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
@@ -30,13 +68,13 @@ function App() {
         <Archive />
         <Footer />
       </section>
-    </PostContextProvider>
+    </PostContext.Provider>
   );
 }
 
 function Header() {
   // 3) consume context value
-  const { onClearPosts } = usePosts();
+  const { onClearPosts } = useContext(PostContext);
   return (
     <header>
       <h1>
@@ -52,7 +90,7 @@ function Header() {
 }
 
 function SearchPosts() {
-  const { searchQuery, setSearchQuery } = usePosts();
+  const { searchQuery, setSearchQuery } = useContext(PostContext);
   return (
     <input
       value={searchQuery}
@@ -63,7 +101,7 @@ function SearchPosts() {
 }
 
 function Results() {
-  const { posts } = usePosts();
+  const { posts } = useContext(PostContext);
   return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
@@ -77,7 +115,7 @@ function Main() {
 }
 
 function Posts() {
-  const { posts } = usePosts();
+  const { posts } = useContext(PostContext);
   return (
     <section>
       <List posts={posts} />
@@ -88,7 +126,7 @@ function Posts() {
 function FormAddPost() {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
-  const { onAddPost } = usePosts();
+  const { onAddPost } = useContext(PostContext);
 
   const handleSubmit = function (e) {
     e.preventDefault();
@@ -134,7 +172,7 @@ function Archive() {
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
     Array.from({ length: 10000 }, () => createRandomPost())
   );
-  const { onAddPost } = usePosts();
+  const { onAddPost } = useContext(PostContext);
 
   const [showArchive, setShowArchive] = useState(false);
 
